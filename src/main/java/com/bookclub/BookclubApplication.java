9d1165f6c3fe08add6421f974e7c6a83b4c5e8d8
Vani <vachukkaluru@my365.bellevue.edu>
2022-03-20 22:47:02 -0500

    Added citations

--- src/main/java/com/bookclub/BookclubApplication.java
+++ src/main/java/com/bookclub/BookclubApplication.java
@@ -1,3 +1,10 @@
+
+/*
+    Krasso, R., (2022). CIS 530 Server Side Development. Bellevue University, all  rights reserved.
+    URL: https://github.com/buwebdev/cis-530
+    Created Date: 03/20/2022
+    Purpose: Main class for Bookclub Spring Boot application
+*/
 package com.bookclub;
 
 import org.springframework.boot.SpringApplication;
--- src/test/java/com/bookclub/BookclubApplicationTests.java
+++ src/test/java/com/bookclub/BookclubApplicationTests.java
@@ -1,3 +1,10 @@
+/*
+    Krasso, R., (2022). CIS 530 Server Side Development. Bellevue University, all  rights reserved.
+    URL: https://github.com/buwebdev/cis-530
+    Created Date: 03/20/2022
+    Purpose: Bookclub Spring Boot application
+*/
+
 package com.bookclub;
 
 import org.junit.jupiter.api.Test;
